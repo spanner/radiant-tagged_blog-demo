88344da626c525038479093e56348d14c2a12eb6
spanner <will@spanner.org>
2011-07-09 22:24:31 +0100

    latest reader/forum/taggable

--- public/javascripts/forum.js
+++ public/javascripts/forum.js
@@ -9,54 +9,63 @@
 */
 
 (function($) { 
+  $.ajaxSettings.accepts.html = $.ajaxSettings.accepts.script;
 
   function RemoteAction (url, holder) {
     var self = this;
-		$.extend(self, {
-		  url: url,
-		  holder: holder,
-		  container: null,
-		  form: null,
+    $.extend(self, {
+      url: url,
+      holder: holder,
+      container: null,
+      form: null,
 
-		  fetch: function () {
-		    if (self.showing()) self.hide();
+      fetch: function () {
+        if (self.showing()) self.hide();
         else if (self.form) self.show();
         else {
           self.wait();
-          $.get(self.url, self.step);  
+          $.get(self.url, self.step, 'html');  
         }
       },
       submit: function (e) {
         var ajaxable = true;
+        // file to upload means not ajaxable at all
         self.container.find('input:file').each(function () {
           var file = $(this).val();
           if (file && file != "") ajaxable = false;
         });
+        // presence of title field means not ajaxable because that appears elsewhere on the page
+        if (self.container.find('input:text').length > 0) ajaxable = false;
+
         if (ajaxable) {
           e.preventDefault();
-          self.form.find('textarea.toolbarred').read_editor();
-          $.post(self.form.attr('action'), self.form.serialize(), self.step);  
+          var editor = $(this).data('editor');
+          self.form.find('textarea').each(function (box) {
+            var editor = $(box).data('editor');
+            if (editor) editor.save();
+          });
+          $.post(self.form.attr('action'), self.form.serialize(), self.step, 'html');  
         } else {
-          return true;  // allow event through so that uploads are sent by normal HTTP POST
+          return true;  // allow event through so that form is sent by normal HTTP POST
                         // toolbar is read in onSubmit
         }
       },
       step: function (results) {
         self.unwait();
         if (results) self.container.html(results);
+        self.container.find('.cancel').click(self.cancel);
         self.form = self.container.find('form');
         if (self.form.length > 0) {
           self.form.submit(self.submit);
-          self.form.find('div.upload_stack').upload_stack();
-          self.form.find('a.cancel').click(self.cancel);
-          self.form.find("textarea.toolbarred").add_editor({});
+          self.form.init_forum();
           self.show();
         } else {
+          $(results).init_forum();
           holder.replaceWith(results);
         }
       },
       cancel: function (event) {
-        squash(event);
+        if (event) event.preventDefault();
         self.unwait();
         self.hide();
       },
@@ -78,31 +87,31 @@
       unwait: function () {
         holder.unwait();
       }
-		});
-		self.container = $('<div class="remote_form" />').hide();
-		self.holder.append(self.container);
+    });
+    self.container = $('<div class="remote_form" />').hide();
+    self.holder.append(self.container);
   }
   
-	function ActionHolder(container, conf) {   
+  function ActionHolder(container, conf) {   
     var self = this;
-		$.extend(self, {
-		  container: container,
-		  wrapper: container.find('.wrapper'),
-		  actions: {},
-		  initActions: function () {
-		    self.actions = {};
-    		self.container.find('a.remote').each(function () {
-    		  var a = $(this);
-    		  var href = a.attr('href');
+    $.extend(self, {
+      container: container,
+      wrapper: container.find('.wrapper'),
+      actions: {},
+      initActions: function () {
+        self.actions = {};
+        self.container.find('a.remote').each(function () {
+          var a = $(this);
+          var href = a.attr('href');
           self.addAction(href);
           a.click(function (event) {
-            squash(event);
+            if (event) event.preventDefault();
             a.addClass('waiting');
             self.showAction(href);
           });
           if (a.is('.autoload')) self.showAction(href);
-    		});
-		  },
+        });
+      },
       addAction: function (url) {
         if (!self.actions[url]) self.actions[url] = new RemoteAction(url, self);
         return self.actions[url];
@@ -127,7 +136,7 @@
         self.wrapper.hide();
       },
       toggle: function (event) {
-        squash(event);
+        if (event) event.preventDefault();
         if (self.wrapper.is(":visible")) self.hide();
         else self.show();
       },
@@ -139,23 +148,23 @@
         self.container.removeClass('waiting');
         self.container.find('a.remote').removeClass('waiting');
       }
-		});
-		self.initActions();
-	}
-	
-	$.fn.enable_remote_actions = function(conf) {
-		this.each(function() {			
-			new ActionHolder($(this), conf);
-		});
-		return this;
-	};
+    });
+    self.initActions();
+  }
+  
+  $.fn.enable_remote_actions = function(conf) {
+    this.each(function() {
+      new ActionHolder($(this), conf);
+    });
+    return this;
+  };
 
-	function HideablePost(container, conf) {   
+  function HideablePost(container, conf) {   
     var self = this;
-		$.extend(self, {
-		  head: container.find('.post_header'),
-		  body: container.find('.post_body'),
-		  shower: null,
+    $.extend(self, {
+      head: container.find('.post_header'),
+      body: container.find('.post_body'),
+      shower: null,
       show: function () {
         self.body.slideDown();
         self.shower.text('Hide');
@@ -165,36 +174,36 @@
         self.shower.text('Show');
       },
       toggle: function (event) {
-        squash(event);
+        if (event) event.preventDefault();
         if (self.body.is(":visible")) self.hide();
         else self.show();
       }
-		});
+    });
     if ($('a.prev_page').length > 0) {
-  	  self.shower = $('<a href="#" class="shower">Hide</a>').appendTo(self.head.find('p.context'));
+      self.shower = $('<a href="#" class="shower">Hide</a>').appendTo(self.head.find('p.context'));
       self.shower.click(self.toggle);
       self.hide();
     }
-	}
+  }
 
-	$.fn.hideable_post = function() { 
-		this.each(function() {			
-			new HideablePost($(this));
-		});
-		return this;
-	};
+  $.fn.hideable_post = function() { 
+    this.each(function() {      
+      new HideablePost($(this));
+    });
+    return this;
+  };
 
-	function UploadStack(container) {   
+  function UploadStack(container) {   
     var self = this;
-		$.extend(self, {
-		  container: container,
+    $.extend(self, {
+      container: container,
       attachments_list: container.find('ul.attachments'),
       uploads_list: container.find('ul.uploads'),
       selector: container.find('div.selector'),
       file_field: container.find('div.selector').find('input'),
       
-			addUpload: function(event) {
-        squash(event);
+      addUpload: function(event) {
+        if (event) event.preventDefault();
         var upload_field = self.file_field.clone();
         var nest_id = self.attachmentCount() + self.uploadCount();  // nb. starts at zero so this total is +1
         var container = $('<li class="attachment">' + upload_field.val() + '</li>');
@@ -225,67 +234,97 @@
     });
     self.attachments_list.find('li').add_remover();
     self.file_field.change(self.addUpload);
-	}
-	
-	$.fn.upload_stack = function(conf) { 
-		this.each(function() {			
-			el = new UploadStack($(this), conf);
-		});
-		return this;
-	};
+  }
+  
+  $.fn.upload_stack = function(conf) { 
+    this.each(function() {      
+      el = new UploadStack($(this), conf);
+    });
+    return this;
+  };
 
-	$.fn.add_remover = function() { 
-	  this.each(function() {
-  	  var self = $(this);
+  $.fn.add_remover = function() { 
+    this.each(function() {
+      var self = $(this);
       var remover = $('<a href="#" class="remove">remove</a>');
       remover.click(function (event) { 
-        squash(event);
+        if (event) event.preventDefault();
         self.slideUp('500', function() { 
           self.find('input.checkbox').attr('checked', true);
           self.find('input.filefield').remove();
         }); 
       });
       self.append(remover);
-	  });
-		return self;
-	};
+    });
+    return self;
+  };
 
-	$.fn.add_editor = function() { 
-		this.each(function() { 
-		  var self = $(this);
-      var editor = new punymce.Editor({
-        id : self.attr('id'),
-        plugins : 'Link,Image,Emoticons,EditSource',
-        toolbar : 'bold,italic,link,unlink,image,emoticons,editsource',
-        width : 510,
-  			height : 375,
-        resize : true
+  $.fn.add_editor = function() { 
+    if(window.punymce !== undefined) {
+      this.each(function() {
+        var self = $(this);
+        var editor = new punymce.Editor({
+          id : self.attr('id'),
+          plugins : 'Link,Image,Emoticons,EditSource',
+          toolbar : 'bold,italic,link,unlink,image,emoticons,editsource',
+          width : 510,
+          height : 375,
+          resize : true
+        });
+        self.data('editor', editor);
       });
-      self.data('editor', editor);
-		});
-		return this;
-	};
+    }
+    return this;
+  };
 
-	$.fn.read_editor = function() { 
-		this.each(function() { 
-		  var self = $(this);
-		  if (self.data('editor')) {
-		    self.val(self.data('editor').getContent());
-		  }
-		});
-		return this;
-	};
-
+  $.fn.capture_search = function() { 
+    this.each(function() { 
+      var self = $(this);
+      var target = $('#results');
+      if (target.length >= 1) {
+        self.submit(function (e) {
+          if(e) e.preventDefault();
+          self.addClass("waiting");
+          target.addClass("waiting");
+          target.load(self.attr('action'), self.serialize(), function () { 
+            self.removeClass("waiting"); 
+            target.removeClass("waiting");
+          });  
+        });
+      } else {
+        return true;
+      }
+    });
+    return this;
+  };
+  
+  $.fn.add_rte = function() { 
+    this.cleditor({
+      width: 630,
+      height: 400,
+      controls: "bold italic underline strikethrough size removeformat | bullets numbering outdent indent | icon image link unlink | source"
+    });
+  };
+  
+  $.fn.init_forum = function () {
+    this.each(function() { 
+      var self = $(this);
+      self.find(".post").enable_remote_actions();
+      self.find(".new_post").enable_remote_actions();
+      self.find(".post.first").hideable_post();
+      self.find(".upload_stack").upload_stack();
+      self.find(".forum_search").capture_search();
+      self.find("textarea.rte").add_rte();
+    });
+    return this;
+  };
 })(jQuery);
 
 $(function() {
-  $(".post").enable_remote_actions({});
-  $(".new_post").enable_remote_actions({});
-  $(".post.first").hideable_post({});
-  $(".upload_stack").upload_stack({});
-  $(".toolbarred").add_editor({});
+  $(document).init_forum();
+  
   $("input:submit").live('click', function (event) {
-	  var self = $(this);
+    var self = $(this);
     self.after('<span class="waiting">Please wait</span>');
     self.hide();
     return true;
